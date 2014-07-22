b18ed8891de1045045094e93075873fd73f223f7
andreashuebner <andreas.huebnerh@gmail.com>
2014-07-23 00:59:49 +0100

    Push after first tests on exceptions

--- sps.js
+++ sps.js
@@ -9,17 +9,49 @@ eventCallback: Callback function to call
 }
 */
 
+//helper functions, not part of public API
+sps._isString=function(valueToCheck) {
+
+    return (typeof(valueToCheck) === "string");
+};
+sps._isFunction=function(valueToCheck) {
+
+    return (typeof(valueToCheck) === "function");
+};
+
+sps._isObject=function(valueToCheck) {
+    var stringify={}.toString.apply(valueToCheck);
+    return (stringify === "[object Object]");
+};
+
 /**
  * Function to subscribe objects to custom events
  *An object can be substribed to several custom events
  *@example
  *testobject1.spson("testevent1",function(event) {
  *console.log("testevent1 triggered");
- *}
+ *});
+ *@example
+ *or use sps (SimplePubSub) object directly
+ *sps.spson("testevent2",function(event) {
+ *console.log("testevent2 triggered");
+ });
  * @param {string} eventName - The name of the custom event that object should be subscribed to
  * @param {function} callbackFunction - Callback function that will be called when sps or any object emits the custom event
  */
-
-Object.prototype.spson(eventName,callbackFunction) {
+Object.prototype.spson=function(eventName,callbackFunction) {
+    var that=this;
+    if (sps._isString(eventName) === false) {
+        throw new Error("spson method expects first parameter to be a string");
+    }
+    //check that "that" refers to object
+    if (sps._isObject(that) === false) {
+         throw new Error("spson method needs to be called on object");
+    }
+    //check that callbackFunction is of type function
+     if (sps._isFunction(callbackFunction) === false) {
+         throw new Error("spson method expects second parameter to be a function");
+    }
+    
     
 };
--- tests/spstests.js
+++ tests/spstests.js
@@ -9,7 +9,7 @@ var testObject3;
 var testObject4;
 var testObject5;
 
-QUnit.module( "Testing Simple Pub Sub subscribe function", {
+QUnit.module("Testing Simple Pub Sub subscribe function", {
   setup: function() {
     testObject1=new TestObject(1);
     testObject2=new TestObject(2);
@@ -25,5 +25,54 @@ QUnit.module( "Testing Simple Pub Sub subscribe function", {
     delete testObject5;
   }
 });
+
+
+
+
+test( "Calling spson method without string as first parameter raises error", function() {
+     
+       throws(
+        function() {
+            testObject1.spson(1,function(event) {
+                console.log("Testevent1 triggered");
+            });
+        },
+        function(error) {
+            
+            return error.message === "spson method expects first parameter to be a string";
+        }
+    );
+     
+    });
+    
+    test( "Calling spson method on non object raises error", function() {
+     
+       throws(
+        function() {
+            [].spson("testevent1",function(event) {
+                console.log("Testevent1 triggered");
+            });
+        },
+        function(error) {
+            
+            return error.message === "spson method needs to be called on object";
+        }
+    );
+     
+    });
+    test( "Calling spson method without function as second parameter raises error", function() {
+     
+       throws(
+        function() {
+            testObject1.spson("testevent1",{});
+        },
+        function(error) {
+            
+            return error.message === "spson method expects second parameter to be a function";
+        }
+    );
+     
+    });
+
 
 

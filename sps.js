ba8029bf1f1b1522bb5aa274cf93452bdfce2810
andreashuebner <andreas.huebnerh@gmail.com>
2014-07-26 18:14:43 +0100

    Push first full working version without build

--- sps.js
+++ sps.js
@@ -1,5 +1,5 @@
 var sps = sps || {}; //sps = SimplePubSub
-sps.arrayEvents={};
+sps.arrayEvents=[];
 /*
 sps.arrayEvents will contain objects with the following structure: 
 {
@@ -24,8 +24,48 @@ sps._isObject=function(valueToCheck) {
     return (stringify === "[object Object]");
 };
 
+sps._objectInArray=function(objectToCheck) {
+//for debugging
+var debug=0;
+if (objectToCheck.eventRef.id === 2) {
+        debug=1;
+     }
+    /*
+    var objectToPush= {
+            eventRef: that,
+            eventName: eventName,
+            eventCallback: callbackFunction
+            
+        };
+     */
+    
+     var identicalObjectFound=0;
+     
+     for (var a=0;a<sps.arrayEvents.length;a++) {
+        var objectToCompare=sps.arrayEvents[a];
+        
+        if (objectToCompare.eventRef === objectToCheck.eventRef && objectToCompare.eventName === objectToCheck.eventName 
+            && String(objectToCheck.eventCallback) === String(objectToCompare.eventCallback)) {
+            
+            identicalObjectFound=1;
+        }
+        
+     }
+     if (debug === 1) {
+      
+     }
+     if (identicalObjectFound === 0) {
+        return false;
+     } else {
+        return true;
+    }
+     
+     
+    
+};
+
 /**
- * Function to subscribe objects to custom events
+ *Subscribe objects to custom events
  *An object can be substribed to several custom events
  *@example
  *testobject1.spson("testevent1",function(event) {
@@ -50,7 +90,69 @@ Object.prototype.spson=function(eventName,callbackFunction) {
     }
     //check that callbackFunction is of type function
      if (sps._isFunction(callbackFunction) === false) {
-         throw new Error("spson method expects second parameter to be a function");
+         throw new Error("spson method expects second parameter to be a callback function");
+    }
+    
+    var objectToPush={};
+    var objectToPush= {
+            eventRef: that,
+            eventName: eventName,
+            eventCallback: callbackFunction
+            
+        };
+        //check if object already exists, in this case do nothing
+        if (sps._objectInArray(objectToPush) === false) {
+         
+            sps.arrayEvents.push(
+                objectToPush
+            );
+    }
+    
+};
+/**
+*Unsubsribe object from a custom event.
+*If an object is subscribed to several custom events, it will only be unsubscribed from the matching custom event
+*@example
+*testobject1.spsoff("testevent");
+*@param {string} eventName - The name of the custom event the object should be unsubsribed from
+*/
+Object.prototype.spsoff=function(eventName) {
+    if (sps._isString(eventName) === false) {
+        throw new Error("spsoff method expect as first parameter the name of the custom event to unsubscribe from as a string.");   
+    }
+    for (var a=0;a<sps.arrayEvents.length;a++) {
+        var objectToCheck=sps.arrayEvents[a];
+        if (objectToCheck.eventName === eventName) {
+            sps.arrayEvents.splice(a, 1);
+        }
+    }
+    
+}
+
+/**
+*Trigger a custom event.
+*Will trigger the callback functions to be called for all objects that are subscribed to this custom event
+*Additionally, an event object can be passed that will be passed on as first argument to the callback functions 
+*@example
+*sps.trigger("testevent1");
+*@example
+*sps.trigger("testevent2",{"eventsource": "testobject"};
+*@param {string} eventName - The name of the custom event to be triggered
+*@param {object} [eventObject] - Optional event object that will be passed as first parameter to all callback functions
+*/
+sps.trigger=function(eventName,eventObject) {
+    if (sps._isString(eventName) === false) {
+        throw new Error("sps trigger expects first argument to be the name of event as string");   
+    }
+    for (var a=0;a<sps.arrayEvents.length;a++) {
+        var objectToCheck=sps.arrayEvents[a];
+        if (objectToCheck.eventName === eventName) {
+            if (sps._isObject(eventObject)) {
+                objectToCheck.eventCallback.call(objectToCheck.eventRef,eventObject);
+            } else {
+                objectToCheck.eventCallback.call(objectToCheck.eventRef,{});
+            }
+        }
     }
     
     
--- tests/spstests.js
+++ tests/spstests.js
@@ -3,19 +3,41 @@ function TestObject(id) {
     this.id=id;
 }
 
+function appendHtml(div,testToAppend) {
+    var existingHtml=document.getElementById(div).innerHTML;
+    existingHtml += "<br>" + testToAppend;
+    document.getElementById(div).innerHTML = existingHtml;
+}
+
 var testObject1;
 var testObject2;
 var testObject3;
 var testObject4;
 var testObject5;
 
-QUnit.module("Testing Simple Pub Sub subscribe function", {
+//custom assertions
+QUnit.assert.divContains = function( div,expectedMessage,expected, message) {
+  var currentHtml=document.getElementById(div).innerHTML;
+  
+  var found=currentHtml.indexOf(expectedMessage);
+  if (found > -1) {
+    var actual=true;
+  } else {
+    var actual=false;
+  }
+  QUnit.push(actual === expected, actual, expected, message);
+
+};
+ 
+
+QUnit.module("Testing Simple Pub Sub functions", {
   setup: function() {
     testObject1=new TestObject(1);
     testObject2=new TestObject(2);
     testObject3=new TestObject(3);
     testObject4=new TestObject(4);
     testObject5=new TestObject(5);
+    document.getElementById("testdiv").innerHTML="";
   },
   teardown: function() {
     delete testObject1;
@@ -23,13 +45,16 @@ QUnit.module("Testing Simple Pub Sub subscribe function", {
     delete testObject3;
     delete testObject4;
     delete testObject5;
+    sps.arrayEvents=[];
+    document.getElementById("testdiv").innerHTML="";
+    delete sps;
   }
 });
 
 
 
 
-test( "Calling spson method without string as first parameter raises error", function() {
+QUnit.test( "Calling spson method without string as first parameter raises error", function() {
      
        throws(
         function() {
@@ -45,7 +70,7 @@ test( "Calling spson method without string as first parameter raises error", fun
      
     });
     
-    test( "Calling spson method on non object raises error", function() {
+   QUnit.test( "Calling spson method on non object raises error", function() {
      
        throws(
         function() {
@@ -60,7 +85,7 @@ test( "Calling spson method without string as first parameter raises error", fun
     );
      
     });
-    test( "Calling spson method without function as second parameter raises error", function() {
+    QUnit.test( "Calling spson method without function as second parameter raises error", function() {
      
        throws(
         function() {
@@ -68,11 +93,194 @@ test( "Calling spson method without string as first parameter raises error", fun
         },
         function(error) {
             
-            return error.message === "spson method expects second parameter to be a function";
+            return error.message === "spson method expects second parameter to be a callback function";
         }
     );
      
     });
+    
+    QUnit.test( "After subscribing testObject1 for testEvent1, array of events contains the right object", function() {
+        testObject1.spson("testevent1",function() {
+          var textToAppend="testevent1 triggered from testobject1";
+          appendHtml("testdiv",textToAppend);
+        });
+        
+        var found=0;
+        //sps.arrayEvents should contain the following dataset
+        /*
+        {
+            eventRef: testObject1,
+            eventName: testevent1,
+            eventCallback: function() {console.log("testevent1 triggered for testObject1");}
+            
+        }
+        */
+        //Todo: Move logic into custom assertion
+        for (var a=0;a<sps.arrayEvents.length;a++) {
+            
+            var element=sps.arrayEvents[a];
+           
+            if (element.eventRef.id === 1 && element.eventName === "testevent1" && typeof(element.eventCallback) === "function") {
+                found += 1;
+            }
+        }
+        QUnit.equal(found,1, "Expect to find subsribed object in event array");
+        
+     
+      
+    });
+    
+    QUnit.test( "Subscribing one object for the same event with same callback function twice should not result in duplicate dataset in eventarray", function() {
+        testObject2.spson("testevent2",function() {
+             var textToAppend="testevent2 triggered from testobject2";
+             appendHtml("testdiv",textToAppend);
+        });
+        testObject2.spson("testevent2",function() {
+             var textToAppend="testevent2 triggered from testobject2";
+             appendHtml("testdiv",textToAppend);
+        });
+        var found=0;
+        //sps.arrayEvents should contain the following dataset
+        
+        //Todo: Move logic into custom assertion
+        for (var a=0;a<sps.arrayEvents.length;a++) {
+            var element=sps.arrayEvents[a];
+           
+            if (element.eventRef.id === 2 && element.eventName === "testevent2" && typeof(element.eventCallback) === "function") {
+                found += 1;
+            }
+        }
+        QUnit.equal(found,1, "Expect to find subsribed object only once in event array, even when subscribed twice");
+        
+     
+      
+    });
+    
+     QUnit.test( "Subscribing one object for the same event with different callback functions should result in two different entries", function() {
+        testObject3.spson("testevent3",function() {
+            var textToAppend="testevent3 triggered from testobject3";
+            appendHtml("testdiv",textToAppend);
+        });
+        testObject3.spson("testevent3",function() {
+            var textToAppend="testevent3 triggered from testobject3 second time";
+            appendHtml("testdiv",textToAppend);
+        });
+        var found=0;
+      
+       
+        //Todo: Move logic into custom assertion
+        for (var a=0;a<sps.arrayEvents.length;a++) {
+            var element=sps.arrayEvents[a];
+            
+           
+            if (element.eventRef.id === 3 && element.eventName === "testevent3" && typeof(element.eventCallback) === "function") {
+                found += 1;
+            }
+        }
+        QUnit.equal(found,2, "Expect to find subsribed object twice in event array when subscribed with different callback functions.");
+  
+    });
+    
+    QUnit.test( "Calling sps trigger method with string as first parameter results in error thrown", function() {
+     
+       throws(
+        function() {
+            sps.trigger({});
+        },
+        function(error) {
+            
+            return error.message === "sps trigger expects first argument to be the name of event as string";
+        }
+    );
+     
+    });
+    
+    QUnit.test( "Subsribing testobject1 to testevent1 and testobject1 to testevent2 and triggering testevent1 will only trigger callback\
+    function for testevent1", function() {
+        testObject1.spson("testevent1",function(eventsource) {
+            //console.log("testevent1 triggered from testobject1 with eventobject " + eventsource.name);
+            var textToAppend="testevent1 triggered from testobject1 with eventobject " + eventsource.name;
+            appendHtml("testdiv",textToAppend);
+        });
+        testObject1.spson("testevent2",function() {
+            console.log("testevent2 triggered from testobject1");
+            var textToAppend="testevent1 triggered from testobject1 with eventobject " + eventsource.name;
+            
+            appendHtml("testdiv",textToAppend);
+            
+        });
+        
+        sps.trigger("testevent1",{"name": "testobject1"});
+        //console.log("Content test div" + " " + document.getElementById("testdiv").innerHTML);
+        //now testdiv should contain the message
+         QUnit.assert.divContains("testdiv","testevent1 triggered from testobject1 with eventobject testobject1",true,"testdiv contains message from object1 with testevent1");
+         QUnit.assert.divContains("testdiv","testevent1 triggered from testobject1 with eventobject testobject2",false,"testdiv does not contain message from object1 with testevent2");
+    
+  });  
+  
+   QUnit.test( "Calling spsoff method without string as first parameter results in error thrown", function() {
+     
+       throws(
+        function() {
+             var testObject1=new TestObject(1);
+             testObject1.spsoff();
+        },
+        function(error) {
+            
+            return error.message === "spsoff method expect as first parameter the name of the custom event to unsubscribe from as a string.";
+        }
+    );
+     
+    });
+    
+    QUnit.test( "Subscribing testobject1 to testevent1 and testobject2 to testevent2 will trigger both functions", function() {
+        testObject1.spson("testevent1",function(eventsource) {
+          
+            var textToAppend="testevent1 triggered from testobject1 with eventobject " + eventsource.name;
+            appendHtml("testdiv",textToAppend);
+        });
+        testObject2.spson("testevent2",function(eventsource) {
+            console.log("testevent2 triggered from testobject2");
+            var textToAppend="testevent2 triggered from testobject2 with eventobject " + eventsource.name;
+            
+            appendHtml("testdiv",textToAppend);
+            
+        });
+        
+        sps.trigger("testevent1",{"name": "testobject1"});
+        sps.trigger("testevent2",{"name": "testobject2"});
+        QUnit.assert.divContains("testdiv","testevent1 triggered from testobject1 with eventobject testobject1",true,"testdiv contains message from object1 with testevent1");
+        QUnit.assert.divContains("testdiv","testevent2 triggered from testobject2 with eventobject testobject2",true,"testdiv contains message from object2 with testevent2");
+        
+    
+  });  
+  
+  QUnit.test( "Subscribing testobject1 to testevent1 and testobject2 to testevent2 and then unsubscribing testobject 1 will only trigger testobject2", function() {
+        testObject1.spson("testevent1",function(eventsource) {
+          
+            var textToAppend="testevent1 triggered from testobject1 with eventobject " + eventsource.name;
+            appendHtml("testdiv",textToAppend);
+        });
+        testObject2.spson("testevent2",function(eventsource) {
+            console.log("testevent2 triggered from testobject2");
+            var textToAppend="testevent2 triggered from testobject2 with eventobject " + eventsource.name;
+            
+            appendHtml("testdiv",textToAppend);
+            
+        });
+        
+        testObject1.spsoff("testevent1");
+        
+        sps.trigger("testevent1",{"name": "testobject1"});
+        sps.trigger("testevent2",{"name": "testobject2"});
+        QUnit.assert.divContains("testdiv","testevent1 triggered from testobject1 with eventobject testobject1",false,"testdiv contains message from object1 with testevent1");
+        QUnit.assert.divContains("testdiv","testevent2 triggered from testobject2 with eventobject testobject2",true,"testdiv contains message from object2 with testevent2");
+        
+    
+  });  
+    
+    
+    
 
 
 
